6dce8cf909dd82be7a78c62f255c9e705e0ff6b7
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-21 17:39:32 +0100

    add snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,25 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+const soccerTeams = [
+    { nome: "Blue Lock Eleven", punti: 0, falliSubiti: 0 },
+    { nome: "Japan U-20", punti: 0, falliSubiti: 0 },
+    { nome: "Manshine City", punti: 0, falliSubiti: 0 },
+    { nome: "FC Barcha", punti: 0, falliSubiti: 0 },
+    { nome: "Bastard München", punti: 0, falliSubiti: 0 },
+    { nome: "Ubers", punti: 0, falliSubiti: 0 }
+  ];
+
+const teamsFalli = [];
+
+for (let i = 0; i < soccerTeams.length; i++) {
+    
+    soccerTeams[i].punti = Math.floor(Math.random() * 20)
+    soccerTeams[i].falliSubiti = Math.floor(Math.random() * 20)
+    teamsFalli.push(soccerTeams[i].nome, soccerTeams[i].falliSubiti)
+
+}
+
+console.log(soccerTeams)
+console.log(teamsFalli)
