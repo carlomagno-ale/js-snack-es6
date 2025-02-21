4114510eb1c8f421db5491c5f438ced79e691ea6
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-21 15:19:26 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,23 @@
+/* Snack 1
+Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore. 
+*/
+
+const bike = [
+    {
+        name: 'Mountain Bike',
+        weight: 12
+    },
+    {
+        name: 'BMX',
+        weight: 10
+    },
+    {
+        name: 'Touring',
+        weight: 14
+    },
+    {
+        name: 'Tandem',
+        weight: 20
+    }
+]
